cd932ad45600e4705f7c1c9b97515f57e7b0822f
Enora Nguyen Huu <enora.nh@gmail.com>
2024-08-19 21:32:47 +0200

    create components folder

---
+++ components/disqus.js
@@ -0,0 +1,8 @@
+fetch('disqus.html')
+.then(res => res.text())
+.then(text => {
+    let old_element = document.querySelector("script#replace_with_disqus");
+    let new_element = document.createElement("div");
+    new_element.innerHTML = text;
+    old_element.parentNode.replaceChild(new_element,old_element);
+})
