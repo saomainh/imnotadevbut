0ae314317f5a20dd549ee48e047967e1f12ead42
Enora Nguyen Huu <enora.nh@gmail.com>
2024-08-19 22:10:33 +0200

    reset all

--- nav.js
+++
@@ -1,8 +0,0 @@
-fetch('nav.html')
-.then(res => res.text())
-.then(text => {
-    let old_element = document.querySelector("script#replace_with_navbar");
-    let new_element = document.createElement("div");
-    new_element.innerHTML = text;
-    old_element.parentNode.replaceChild(new_element,old_element);
-})
