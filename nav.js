d3afda79bbc6b217776ceb7a2e63a1055f3a9752
Enora Nguyen Huu <enora.nh@gmail.com>
2024-08-19 21:51:19 +0200

    remove components please please please

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
-fetch('./disqus.html')
+fetch('nav.html')
 .then(res => res.text())
 .then(text => {
-    let old_element = document.querySelector("script#replace_with_disqus");
+    let old_element = document.querySelector("script#replace_with_navbar");
     let new_element = document.createElement("div");
     new_element.innerHTML = text;
     old_element.parentNode.replaceChild(new_element,old_element);
